68a7524bf25919638ac7e83c40e55aa64a98d71f
Albert John <aljohme@gmail.com>
2025-08-04 18:19:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/data-management/page.tsx
+++ src/app/(app)/data-management/page.tsx
@@ -13,6 +13,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { handleDataManagementAction } from '@/lib/actions';
 import { useToast } from '@/hooks/use-toast';
