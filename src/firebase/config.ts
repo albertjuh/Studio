9bd5488db326e4b6a701a4f5750446c7e57133cd
Albert John <aljohme@gmail.com>
2026-02-13 11:50:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "nutshell-insights",
+  "appId": "1:1003963455184:web:58e18dd6f39bb7192af0d5",
+  "apiKey": "AIzaSyBvBrjkiuTawCgfw9qLtILZAYPNOTV-VY8",
+  "authDomain": "nutshell-insights.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1003963455184"
+};
